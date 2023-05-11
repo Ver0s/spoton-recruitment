9f00dfe52b2dad79da2fe7ce1922768f35c4c023
Arek <arek.stachyra41@gmail.com>
2023-05-11 12:51:59 +0200

    chore: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,28 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'standard-with-typescript',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json'],
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ['react'],
+	rules: {
+		'@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'react/react-in-jsx-scope': 'off',
+	},
+};
